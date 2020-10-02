554d67bdc5196352d6fd38c159f58a58203f285a
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-10-02 15:18:53 -0400

    update

--- src/test/java/test/office_hours/ExcellIO.java
+++ src/test/java/test/office_hours/ExcellIO.java
@@ -4,6 +4,8 @@ import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Arrays;
 
 public class ExcellIO {
 
@@ -56,16 +58,47 @@ public class ExcellIO {
 
             // to print entire table
 
+            String [][] dataTable = new String[rowNumber][columnNumber];
             for (int rowIndex = 0; rowIndex < rowNumber; rowIndex++  ){
                 for (int columnIndex = 0; columnIndex < columnNumber; columnIndex++){
-                    System.out.print(sheet.getRow(rowIndex).getCell(columnIndex) + " ");
+                    String value = sheet.getRow(rowIndex).getCell(columnIndex).getStringCellValue();
+                    System.out.format("%15s", value);
+                    dataTable[rowIndex][columnIndex] = value;
                 }
                 System.out.println();
             }
 
+            System.out.println(Arrays.deepToString(dataTable));
 
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void writeIntoExcelFile(){
+
+        // input stream - read
+        // output stream - write
+        // NOTE: close Excel file during writing
+        try(FileInputStream fileInputStream = new FileInputStream("VytrackTestUsers.xlsx")){
+
+            Workbook workbook = WorkbookFactory.create(fileInputStream);
+            Sheet sheet = workbook.getSheet("QA1-short");
+            Row row = sheet.getRow(1); // get second row
+            // write "passed" to 2nd row 6th column
+            row.getCell(5).setCellValue("PASSED");
+
+            FileOutputStream fileOutputStream = new FileOutputStream("VytrackTestUsers.xlsx");
+            workbook.write(fileOutputStream); // to write updates to the file
+            fileOutputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }
